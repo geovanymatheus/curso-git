fc024164ef1e213843dd39ca40ec4818785d0aa2
Geovany Matheus F. dos Santos <matheus.geovany@gmail.com>
2021-03-02 10:03:41 -0300

    Inserindo mensagem de Bom dia

--- ProjetoNovo/Program.cs
+++ ProjetoNovo/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoNovo
         static void Main(string[] args)
         {
             Console.WriteLine("Projeto Git");
+
+            Console.WriteLine("Bom dia!");
         }
     }
 }
